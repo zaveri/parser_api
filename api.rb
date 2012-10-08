08e3ee7f5eb6101a1c90c653995322fe368f4863
Muzzammil Zaveri <m.h.zaveri@gmail.com>
2012-10-07 18:17:54 -0700

    new api to parse out email bodies

--- api.rb
+++ api.rb
@@ -5,6 +5,41 @@ require 'tmail'
 require 'gmail'
 require 'nokogiri'
 require 'open-uri'
+require'date'
+
+
+
+def remove_time(node)
+  months = Date::ABBR_MONTHNAMES
+  months.each { |m|
+    if(node.content.include? "On #{m}")
+      node.remove
+    end 
+  }
+end
+
+def remove_sent_from_iphone(node)
+  if(node.content.include? "Sent from my iPhone")
+    node.content = node.content.sub("Sent from my iPhone", "")
+  end
+end
+
+def remove_blocquote_cite(doc)
+  doc.css("blockquote").each {|n|
+    blockquoteAttr = n.attributes["type"].to_s
+    if blockquoteAttr == "cite"
+      n.remove
+    end
+  }
+end
+
+def remove_signature(node)
+  if(node.content.include?"--")
+    node.remove
+  end
+end
+
+
 
 get '/' do
   return "GANGNAM SYLE"
@@ -18,7 +53,19 @@ end
 post '/parse.json' do
   htmlBody = params[:html_body]
   doc = Nokogiri::HTML.parse(htmlBody, 'UTF-8')
-  doc.css("div.gmail_quote").remove
+  
+  doc.css("div.gmail_quote, blockquote.gmail_quote").remove
+  
+  # remove from my iPhone, sent on,  blockquote cite (reference is email from ED)
+
+  doc.css("div").each {|n|
+    remove_sent_from_iphone(n)
+    remove_time(n)
+    remove_signature(n)
+  }
+  
+  remove_blocquote_cite(doc)
+      
   content_type :json
   return {:parsed_email => doc}.to_json
 end
